5dc9b5a5156fb3256afc5e63901169c9ce66d21f
李小保 <lixiaobao@mininglamp.com>
2019-07-23 08:47:32 +0800

    init

---
+++ src/main/java/org/lxb/algorithmic/Main.java
@@ -0,0 +1,9 @@
+package org.lxb.algorithmic;
+
+/**
+ * @author lixiaobao
+ * @create 2019-07-23 8:45 AM
+ **/
+
+public class Main {
+}
---
+++ src/main/java/org/lxb/algorithmic/list/skiplist/SkipNode.java
@@ -0,0 +1,9 @@
+package org.lxb.algorithmic.list.skiplist;
+
+/**
+ * @author lixiaobao
+ * @create 2019-07-23 8:47 AM
+ **/
+
+public class SkipNode {
+}
